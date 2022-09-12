a4f9ec4c9556c2352a7890fd7fe1b961c777391e
Nathan <nathan.wthamilton48@gmail.com>
2022-09-11 23:47:39 -0500

    setting up game state

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-let initialState;
-
-
-
+//creates the game state object
+const gameState = {};
+  
+//populates the gameState object with starting info 
 function buildInintialState() {
-
+    gameState.players = ["x", "o"];
+    gameState.board = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ];
 };
+
+
+const boardElem = document.querySelector("#board");
+
+buildInintialState();
 
 function renderState() {
 
