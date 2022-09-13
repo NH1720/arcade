4e3e583b0d1bb3f3b2dad96064c1c6280b18c3d0
Nathan <nathan.wthamilton48@gmail.com>
2022-09-12 22:29:20 -0500

    creating inital board

--- app.js
+++ app.js
@@ -5,14 +5,28 @@ const gameState = {};
 function buildInintialState() {
     gameState.players = ["x", "o"];
     gameState.board = [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null]
+        null, null, null,
+        null, null, null,
+        null, null, null
     ];
 };
 
-
+buildInintialState();
 const boardElem = document.querySelector("#board");
+for (let i = 0; i < gameState.board.length; i++) {
+    let card = gameState.board[i];
+    const cardElem = document.createElement("div");
+    cardElem.classList.add("card");
+    boardElem.appendChild(cardElem);
+}
+// for (let i = 0; i < gameState.board.length; i++) {
+//     let value = gameState.board[i]
+//     for (let j = 0; j < iIndex.length; j++) {
+//         let innerValue = value[j];
+//         console.log(innerValue); 
+//     }
+// }
+
 
 buildInintialState();
 
