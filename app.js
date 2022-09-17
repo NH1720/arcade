451e263f5e68150ed0e491331ba96c5486c9d4f8
Nathan <nathan.wthamilton48@gmail.com>
2022-09-17 08:35:21 -0500

    friday

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 //creates the game state object
-const gameState = {};
+// const gameState = {};
   
 //populates the gameState object with starting info 
 function buildInintialState() {
     gameState.board = [
-        { value: null , isTurned: false},
-        { value: null , isTurned: false},
-        { value: null , isTurned: false},
-        { value: null , isTurned: false},
-        { value: null , isTurned: false},
-        { value: null , isTurned: false},
-        { value: null , isTurned: false},
-        { value: null , isTurned: false},
-        { value: null , isTurned: false},
+        { value: null, isTurned: false},
+        { value: null, isTurned: false},
+        { value: null, isTurned: false},
+        { value: null, isTurned: false},
+        { value: null, isTurned: false},
+        { value: null, isTurned: false},
+        { value: null, isTurned: false},
+        { value: null, isTurned: false},
+        { value: null, isTurned: false},
     ];
-    gameState.currentPlayer = ["", ""];
+    gameState.players = ["", ""];
     gameState.currentPlayerIdx = 0;
     gameState.getCurrentPlayer = function () {
-        return gameState.currentPlayer[gameState.currentPlayerIdx];
+        return gameState.players[gameState.currentPlayerIdx];
     };
-    gameState.scores = [0, 0];
     gameState.lastTurnedIdx = -1;
 };
 
 //selectors
-const boardElem = document.querySelector("#board");
-const playerTurnElm = document.querySelector("#player-turn");
-const scoreElm = document.querySelector('#score');
+// const boardElem = document.querySelector("#board");
+// const playerTurnElm = document.querySelector("#player-turn");
+// const scoreElm = document.querySelector('#score');
 
 //game logic
 function changeTurn() {
@@ -58,7 +57,7 @@ function renderPlayer() {
 //renderScore
 function renderScore() {
     scoreElm.innerHTML = `
-    ${gameState.currentPlayer[0]}: ${gameState.scores[0]}</div> 
+    ${gameState.currentPlayer[0]}: ${gameState.scores[0]} 
     ${gameState.currentPlayer[1]}: ${gameState.scores[1]} 
     `;
 };
@@ -88,37 +87,114 @@ function takeTurn(cardIdx) {
 };
 
 //listeners
-boardElem.addEventListener('click', function (event) {
-    if (event.target.className !== 'space') {
-        return;
-    };
-    console.log('event', event.target);
-    const cardIdx = event.target.dataset.index;
-    const card = gameState.board[cardIdx];
-    if (card.isTurned) {
-        return;
-    };
-    gameState.board[cardIdx].isTurned = true;
-    changeTurn();
-    console.log('currentPlayer', gameState.getCurrentPlayer());
-    render();
-});
+// boardElem.addEventListener('click', function (event) {
+//     if (event.target.className !== 'space') {
+//         return;
+//     };
+//     console.log('event', event.target);
+//     const cardIdx = event.target.dataset.index;
+//     const card = gameState.board[cardIdx];
+//     if (card.isTurned) {
+//         return;
+//     };
+//     gameState.board[cardIdx].isTurned = true;
+//     changeTurn();
+//     console.log('currentPlayer', gameState.getCurrentPlayer());
+//     render();
+// });
 
 
-playerTurnElm.addEventListener ("click", function(event) {
-    if (event.target.className !== "start") {
-        return;
-    } 
-    const player1Input = document.querySelector("input[name=player1]");
-    const player2Input = document.querySelector("input[name=player2]");
-    gameState.currentPlayer[0] = player1Input.value;
-    gameState.currentPlayer[1] = player2Input.value;
-    render();
-});
+// playerTurnElm.addEventListener ("click", function(event) {
+//     if (event.target.className !== "start") {
+//         return;
+//     } 
+//     const player1Input = document.querySelector("input[name=player1]");
+//     const player2Input = document.querySelector("input[name=player2]");
+//     gameState.currentPlayer[0] = player1Input.value;
+//     gameState.currentPlayer[1] = player2Input.value;
+//     render();
+// });
 
-buildInintialState();
-render();
+// buildInintialState();
+// render();
 
+let main = document.getElementById("main");
 
+function collectInfoForPlayer(player) {
+    let nameLabel = document.createElement("label");
+    nameLabel.setAttribute("for", "player-name");
+    let playerNameInput = document.createElement("input");
+    playerNameInput.setAttribute("id", "player-name");
+    main.appendChild(nameLabel);
+    main.appendChild(playerNameInput);
+
+    let nextButton = document.createElement("button");
+    main.appendChild(nextButton);
+    nextButton.addEventListener("click", () => {
+        let input = document.getElementById("player-name");
+        player.name = input.value;
+    });
+}
+
+function clearMain() {
+    while (main.firstChild) {
+        main.removeChild(main.firstChild);
+    }
+}
+
+let gameConfig = {
+    currentState: "modeSelect",
+    renderCurrentState: () => {
+        switch(this.currentState) {
+            case "modeSelect":
+                let singlePlayerBtn = document.createElement("button");
+                const singlePlayerBtnText = document.createTextNode("One Player");
+                singlePlayerBtn.appendChild(singlePlayerBtnText);
+                singlePlayerBtn.addEventListener("click", () => {
+                    this.players[1].name = "Computer";
+                    this.players[1].type = "computer";
+                    this.setCurrentState("playerInfo");
+                });
+                main.appendChild(singlePlayerBtn);
+
+                let twoPlayerBtn = document.createElement("button");
+                const twoPlayerBtnText = document.createTextNode("Two Players");
+                twoPlayerBtn.appendChild(twoPlayerBtnText);
+                twoPlayerBtn.addEventListener("click", () => {
+                    this.players[1].type = "human";
+                    this.setCurrentState("playerInfo");
+                })
+                main.appendChild(twoPlayerBtn);
+                break;
+            case "playerInfo":
+                console.log("You're in player info")
+                break;
+            case "game":
+                break;
+            default:
+                console.log("hey add this state dude");
+                console.log(this.currentState);
+        }
+    },
+    players: [
+        {
+            name: undefined,
+            type: "human",
+            mark: "X"
+        },
+        {
+            name: undefined,
+            type: undefined,
+            mark: "O"
+        }
+    ],
+    setCurrentState: (state) => {
+        this.currentState = state;
+        clearMain();
+        this.renderCurrentState();
+    },
+};
+
+gameConfig.renderCurrentState();
 
 
